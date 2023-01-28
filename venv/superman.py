21c148bc47242d622bc990c93d085fb00a55bd74
marchello <marchello018@gmail.com>
2023-01-28 21:14:57 +0300

    Процесс стогнации

--- venv/superman.py
+++ venv/superman.py
@@ -1,2 +1,6 @@
-name = input( "Как зовут Диму?")
-age = input("скоко тебе?")
+print('Добро пожаловать к нам в гости')
+print('Мы с тобой')
+password = input("Sex")
+if password == "Sex" :
+    input("Здравствуйте ХУЛЕ")
+input("\n\nВыходи отсюда.")
